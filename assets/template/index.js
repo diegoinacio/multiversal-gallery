6cbbb1f0de3147fc341cb3fa6b42bed12a740186
Diego Inácio <diegodci@gmail.com>
2021-12-06 14:25:45 -0300

    Upload assets template

---
+++ assets/template/index.js
@@ -0,0 +1,28 @@
+import { INDEX } from "../../metadata.mjs";
+
+// * Init main content
+const main = document.querySelector("#main");
+main.innerHTML = "";
+
+for (const [i, a] of INDEX.entries()) {
+  // ! Build article
+  const article = document.createElement("article");
+  article.id = a.id;
+  article.className = "thumb";
+  main.appendChild(article);
+
+  article.innerHTML = `
+  <a href="images/fulls/${a.id}.png" class="image">
+    <img src="images/thumbs/${a.id}.png" />
+  </a>
+  <h2>${a.name}</h2>
+  <p>${a.description}</p>
+  <a
+    href="https://raw.githubusercontent.com/diegoinacio/multiversal-gallery/master/images/fulls/${a.id}.png"
+    class="fa regular fa-expand" 
+    id="bt-full-resolution" 
+    title="view full resolution"
+    target="_blank"
+  ></a>
+  `;
+}
