0a99fc2cf8cb21bd7ea3b7f4381d885dd3d57bc9
Diego Inácio <diegodci@gmail.com>
2021-12-06 14:25:54 -0300

    Upload metadata

---
+++ metadata.mjs
@@ -0,0 +1,32 @@
+export const INDEX = [
+  // ! Volume Rendering of CT scan
+  {
+    id: "volume_rendering_ct_scan",
+    name: "Volume Rendering of CT scan",
+    description: `Three-dimensional volumetric representation of a <em>CT scan</em> based on a data set obtained in <a href="https://www.cancerimagingarchive.net/" target="_blank">Cancer Imaging Archive</a>. Watch the <a href="https://youtu.be/stFDc82hC-A" target="_blank">animation</a>.`,
+  },
+  // ! Multi-gyre flow field
+  {
+    id: "multigyre_vector_field",
+    name: "Multi-gyre flow field",
+    description: `Flow visualization of a multi-gyre system modeled by <em>Quaternions</em> and <em>RBFs</em> over points and vectors. Watch the <a href="https://youtu.be/OsQigOejilQ" target="_blank">animation</a>.`,
+  },
+  // ! 67P/Churyumov-Gerasimenko
+  {
+    id: "67p_churyumov_gerasimenko",
+    name: "67P/Churyumov-Gerasimenko",
+    description: `Beauty representation of the comet 67P/Churyumov-Gerasimenko based on the <a href="https://pdssbn.astro.umd.edu/holdings/ro-c-multi-5-67p-shape-v2.0/dataset.shtml" target="_blank">dataset</a> from <em>NASA's Planetary Data System</em>.`,
+  },
+  // ! Rössler attractor
+  {
+    id: "rossler_attractor",
+    name: "Rössler attractor",
+    description: `Two <a href="https://en.wikipedia.org/wiki/R%C3%B6ssler_attractor" target="_blank">Rössler systems</a> running at the same time with different initial conditions.`,
+  },
+  // ! Lorenz attractor
+  {
+    id: "lorenz_attractor",
+    name: "Lorenz attractor",
+    description: `Two <a href="https://en.wikipedia.org/wiki/Lorenz_system" target="_blank">Lorenz systems</a> running at the same time with different initial conditions.`,
+  },
+];
